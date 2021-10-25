425a3138918f558cddc0c3f60a4a5b6529189346
hyejun-an <61446288+hyejun-an@users.noreply.github.com>
2021-10-25 13:58:14 -0400

    form submission

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
+    const username = loginInput.value;
+    if(username === "") {
+        alert("Please write your name");
+    } else if(username.length > 15) {
+        alert("Your name is too long");
+    }
     console.log(loginInput.value);
     console.log("click!!!");
 }
