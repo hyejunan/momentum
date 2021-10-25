821780163983bdecd32d2b002191fcee6edb5b02
hyejun-an <61446288+hyejun-an@users.noreply.github.com>
2021-10-25 14:06:43 -0400

    events

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginBtn = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginBtn = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
+function onLoginSubmit() {
     const username = loginInput.value;
-    if(username === "") {
-        alert("Please write your name");
-    } else if(username.length > 15) {
-        alert("Your name is too long");
-    }
     console.log(loginInput.value);
-    console.log("click!!!");
 }
 
-loginBtn.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
 
